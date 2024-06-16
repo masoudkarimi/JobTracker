23e6b3c4853edadfbf2d7a852c8ec2805f8115f3
masoud <karimi.masoud5494@gmail.com>
2024-06-16 19:31:45 +0330

    add viewmodel in ContentView.swift and read emitted values

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,32 +2,60 @@ import SwiftUI
 import Shared
 
 struct ContentView: View {
-    @State private var showContent = false
+    @ObservedObject private(set) var viewModel: ViewModel
+    
     var body: some View {
-        VStack {
-            Button("Click me!") {
-                withAnimation {
-                    showContent = !showContent
-                }
-            }
-
-            if showContent {
-                VStack(spacing: 16) {
-                    Image(systemName: "swift")
-                        .font(.system(size: 200))
-                        .foregroundColor(.accentColor)
-                    Text("SwiftUI: \(Greeting().greet())")
-                }
-                .transition(.move(edge: .top).combined(with: .opacity))
+        Button("Click me!") {
+            Task {
+                await viewModel.startObserving()
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding()
     }
 }
 
+
+
+extension ContentView {
+    
+    @MainActor
+    class ViewModel: ObservableObject {
+        @Published var values: [String] = []
+        
+        init() {
+            Task {
+                do {
+                    let db = Database(databaseDriver: DatabaseDriver())
+                    let emittedValues = try await db.getAllApplicationStatusesList()
+                    
+                    if emittedValues.isEmpty {
+                        try await db.insertNewApplicationStatus(name: "Wishlist")
+                        try await db.insertNewApplicationStatus(name: "Applied")
+                        try await db.insertNewApplicationStatus(name: "Interview")
+                    }
+                    
+                    NSLog("Adding items done!")
+                } catch {
+                    NSLog(error.localizedDescription)
+                }
+                
+            
+            }
+        }
+        
+        func startObserving() async {
+            let db = Database(databaseDriver: DatabaseDriver())
+            let emittedValues = await db.getAllApplicationStatuses()
+            for await item in emittedValues {
+                NSLog("Item received \(item)")
+            }
+        }
+        
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: ContentView.ViewModel())
     }
 }
